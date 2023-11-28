de85ced0b44dc8a3bc16a8adf2e3aba3a2cd4f31
Robert Horvat <robert.horvath93@gmail.com>
2023-11-28 23:41:34 +0200

    small edits

--- invoice_package_Robert.py
+++ invoice_package_Robert.py
@@ -5,10 +5,8 @@ from fpdf import FPDF
 from pathlib import Path
 
 
-
-
-
-def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amount_purchased, price_per_unit, total_price):
+def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amount_purchased, price_per_unit,
+             total_price):
     filepaths = glob.glob(f"{invoices_path}/*.xlsx")
     print(filepaths)
 
@@ -17,15 +15,12 @@ def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amo
         pdf = FPDF(orientation="P", unit="mm", format="A4")
         pdf.add_page()
         filename = Path(filepath).stem
-        # stem extract a property of the file
-        # which here will be the name
+
         invoice_nr = filename.split("-")[0]
         date = filename.split("-")[1]
-        # or use invoice_nr, date = filename.split("-")
 
         pdf.set_font(family="Times", size=16, style="B")
         pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
-        # ln=1 indicated that a breakline should be added
 
         pdf.set_font(family="Times", size=16, style="B")
         pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
@@ -33,7 +28,7 @@ def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amo
         df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
         columns = list(df.columns)
-        # converted to list from object type
+
         columns = [item.replace("_", " ").title() for item in columns]
         pdf.set_font(family="Times", size=10, style="B")
         pdf.set_text_color(80, 80, 80)
@@ -51,9 +46,6 @@ def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amo
             pdf.cell(w=30, h=8, txt=str(row[amount_purchased]), border=1)
             pdf.cell(w=30, h=8, txt=str(row[price_per_unit]), border=1)
             pdf.cell(w=30, h=8, txt=str(row[total_price]), border=1, ln=1)
-        # str here is to convert integer values to string
-        # because they are used in a replace() function
-        # and ints cant be used with that function
 
         total_sum = df[total_price].sum()
         pdf.set_font(family="Times", size=10)
@@ -66,20 +58,12 @@ def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amo
 
         pdf.set_font(family="Times", size=10, style="B")
         pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
-        # Add total sum sentence
 
         pdf.set_font(family="Times", size=14, style="B")
         pdf.cell(w=25, h=8, txt=f"Python")
         pdf.image(image_path, w=10)
-        # Add company name and logo
 
         os.makedirs(pdfs_path)
         pdf.output(f"{pdfs_path}/{filename}.pdf")
-        # f string useful here
-        # because we generate files dynamically from multiple files
-
-        # we create a pdf for each excel
-        # that's the logic to use inside for loop
 
     print(df)
-
